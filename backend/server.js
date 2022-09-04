4e4da543a77be84380aec5275ce9e9243f17091b
Iftee97 <tawsifmasrur@gmail.com>
2022-09-04 18:15:10 +0600

    added cors package

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const workoutRoutes = require('./routes/workouts')
 const mongoose = require('mongoose')
+var cors = require('cors')
+
 
 // connect to mongoose
 const main = async () => {
@@ -11,16 +13,26 @@ const main = async () => {
 }
 main().catch(err => console.log(err))
 
+
 // middlewares
 app.use(express.json()) // checks for data in req.body
+
+app.use(cors({
+  origin: 'http://localhost:3000' || 'http://192.168.0.106:3000' || '*',
+  methods: ['GET', 'POST', 'DELETE']
+}))
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
+
+// routes
 app.use('/api/workouts', workoutRoutes)
 
+
+// start server
 app.listen(process.env.PORT, () => {
   console.log(`server listening on port ${process.env.PORT}`)
 })
